79f80e75d2950a0d1d688401880c980dfa89c0ec
Waldemar Kmiecik <waldemar.kmiecik@microsoft.wsei.edu.pl>
2022-11-12 18:03:34 +0100

    Barrels fix

--- Assets/FPS/Scripts/Gameplay/ScreenShake.cs
+++ Assets/FPS/Scripts/Gameplay/ScreenShake.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScreenShake : MonoBehaviour
+namespace Unity.FPS.Gameplay
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ScreenShake : MonoBehaviour
     {
-        
+        private PlayerCharacterController Player;
+        [SerializeField] float duration = 2;
+
+        void Start()
+        {
+            Player = GetComponentInParent<PlayerCharacterController>();
+            StartCoroutine(Shake(2, 0.2f));
+        }
+
+        public IEnumerator Shake(float duration, float magnitude)
+        {
+            yield return new WaitForSeconds(3);
+
+            Vector3 orignalPosition = transform.position;
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                float x = Random.Range(-1f, 1f) * magnitude;
+                float y = Random.Range(-1f, 1f) * magnitude;
+
+                transform.position = new Vector3(x, y, -10f);
+                elapsed += Time.deltaTime;
+                yield return 0;
+            }
+            transform.position = orignalPosition;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
